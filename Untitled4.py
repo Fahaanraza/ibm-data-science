58da4460eac345cf9eae1f7044d7e417875ed221
Fahaanraza <47321056+Fahaanraza@users.noreply.github.com>
2022-04-16 08:08:39 +0530

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# #### Fahaanraza M Walikar
+
+# Software Development Engineer
+
+# _I am interested in data science because, I want to combine the power of AI/ML with the business requirements, to create powerful solutions and provide deep insights into issues an organization needs to solve or discover underlying patterns to discover new potential insights_
+
+# ### The Following Code will generate a random integer between 80 and 100
+
+# In[2]:
+
+
+import random
+
+Random_int = random.randint(80,100)
+
+print(Random_int)
+
+
+# Interested technologies:
+
+# 1. AI-ML/DL
+# 2. ~~cooking.~~
+# 3. [I'm an inline-style link](https://www.google.com)
+
+# In[ ]:
+
+
+
+
